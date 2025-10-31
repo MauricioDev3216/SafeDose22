4550022547cb831abf0fc9d199593e53e25e4337
Aluno São Lucas <aluno.saolucas@ensino.ulbra.net>
2025-10-31 17:55:27 -0300

    Brito: Upando todas as telas bases

--- SafeDose/src/br/ulbra/View/MedicoView.java
+++ SafeDose/src/br/ulbra/View/MedicoView.java
@@ -7,14 +7,14 @@ package br.ulbra.View;
 
 /**
  *
- * @author Vanderlei
+ * @author aluno.saolucas
  */
-public class CadastroView extends javax.swing.JFrame {
+public class MedicoView extends javax.swing.JFrame {
 
     /**
-     * Creates new form CadastroView
+     * Creates new form MedicoView
      */
-    public CadastroView() {
+    public MedicoView() {
         initComponents();
     }
 
@@ -60,20 +60,20 @@ public class CadastroView extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CadastroView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MedicoView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CadastroView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MedicoView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CadastroView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MedicoView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CadastroView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MedicoView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new CadastroView().setVisible(true);
+                new MedicoView().setVisible(true);
             }
         });
     }
